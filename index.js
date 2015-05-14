9f42a94ad18de2a839ce918590d9dbd68d87fbc8
lgausman1 <l.gausman@comcast.net>
2015-05-13 17:37:01 -0700

    installed underscore and body-parser

---
+++ index.js
@@ -0,0 +1,23 @@
+// REQUIREMENTS //
+var express = require("express"),
+    app = express(),
+    path = require("path"),
+    _ = require("underscore"),
+    bodyParser = require("body-parser");
+
+    app.get("/", function(req, res) {
+    	res.send("Hello world");
+    });
+
+    app.listen(3000, function(req, res) {
+    	console.log("server working a-okay");
+    });
+
+    // data
+    var catchphrases =[
+	  {id: 0, word: "bootstrap", definition: "awesome css framework"},
+	  {id: 1, word: "foo", definition: "silly programming variable"},
+	  {id: 2, word: "HTML template", definition: "a cookie-cutter approach to creating HTML pages"},
+	  {id: 3, word: "AJAX", definition: "Asynchronous Javascript And XHTML"},
+	  {id: 4, word: "DOM", definition: "Document Object Model"}
+	];

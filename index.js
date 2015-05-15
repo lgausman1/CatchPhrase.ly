87a9b80124b5b45463b6970a8c644bb8e1a165e8
lgausman1 <l.gausman@comcast.net>
2015-05-14 19:38:05 -0700

    added form and submit functionality, added client-side css

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ var express = require("express"),
     _ = require("underscore"),
     bodyParser = require("body-parser");
 
-    //app.get("/", function(req, res) {
-    //	res.send("Hello world");
-    //});
-
 	app.use(express.static(__dirname + '/public'));
 
-    app.listen(3000, function(req, res) {
-    	console.log("server working a-okay");
-    });
+  // body parser config
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
     // DATA
     var catchphrases =[
@@ -36,10 +33,26 @@ app.get("/", function (req, res){
 app.get("/catchphrases", function (req, res){
   // render catchphrases index as JSON
   res.send(JSON.stringify(catchphrases));
+  console.log(catchphrases);
+});
+
+app.post("/catchphrases", function(req, res) {
+  // find new catchphrase in the req.body
+  var newPhrase = req.body;
+  // get the last id in the array, increment by 1 and assign new id
+  newPhrase.id = catchphrases[catchphrases.length -1].id + 1;
+  //add to the catchphrase array
+  catchphrases.push(newPhrase);
+  // render the created object as json
+  res.send(JSON.stringify(newPhrase));
+  console.log("route is working");
+
 });
 
 
 
 
-
-
+//////// don't touch this!
+app.listen(3000, function(req, res) {
+  console.log("server working a-okay");
+});
--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,26 @@
 $(function(){
   // get and render the phrases
   Phrase.all();
-  console.log("hello");
+  // set the view's behavior
+  View.init();
 });
 
 // // // // // // //
 
 // VIEW OBJECT
 function View() {};
+
+View.init = function() {
+  // catchphrase form submit event listener
+  $('#phrase-form').on('submit', function(e) {
+    e.preventDefault();
+    // format form data into a query string
+    var phraseParams = $(this).serialize();
+    Phrase.create(phraseParams);
+    console.log(phraseParams);
+  });
+};
+
 View.render = function(items, parentId, templateId) {
   // render a template
   var template = _.template($("#" + templateId).html());
@@ -26,3 +39,17 @@ Phrase.all = function() {
     View.render(phrases, "phrase-ul", "phrase-template");
   });
 };
+
+Phrase.create = function(phraseParams) {
+  $.post("/catchphrases", phraseParams).done(function(res) {
+    // once done, re-render all catchphrases
+    Phrase.all();
+  }).done(function(res) {
+      // reset form
+      $("#phrase-form")[0].reset();
+  });
+};
+
+
+
+
